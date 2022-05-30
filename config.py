4f99b23539b2be6d3a51deb0b281f36c84e22fb7
Dima Evan <dimaevan@gmail.com>
2022-05-30 15:09:31 +0300

    Refactor gps_coordinates

---
+++ config.py
@@ -0,0 +1,15 @@
+with open("api_key.txt") as f:
+    api = f.readline()
+
+
+USE_ROUNDED_COORDS = True
+OPENWEATHER_API = api or "7549b3ff11a7b2f3cd25b56d21c83c6a"
+OPENWEATHER_URL = (
+"https://api.openweathermap.org/data/2.5/weather?"
+"lat={latitude}&lon={longitude}&"
+"appid=" + OPENWEATHER_API + "&lang=ru&"
+"units=metric"
+)
+
+if __name__ == "__main__":
+    print(api)
--- gps_coordinates.py
+++ gps_coordinates.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 from subprocess import Popen, PIPE
-
+from typing import Literal
 from exceptions import CantGetCoordinates
+
+import config
 
 @dataclass(slots=True, frozen=True)
 class Coordinates():
@@ -9,14 +11,55 @@ class Coordinates():
     longitude: float
 
 
-def get_coordinates() -> Coordinates:
+def get_gps_coordinates() -> Coordinates:
     """ Return current coordiantes  """
+    coordinates = _get_whereami_coordinates()
+    return _round_coordiantes(coordinates)
+
+
+def _get_whereami_coordinates() -> Coordinates:
+    whereami_output = _get_whereami_output()
+    coordinates = _parse_coordinates(whereami_output)
+    return coordinates
+
+
+def _get_whereami_output() -> bytes:
     process = Popen(['whereami'], stdout=PIPE)
     (output, err) = process.communicate()
     exit_code = process.wait()
     if err is not None or exit_code != 0:
         raise CantGetCoordinates
-    output_lines = output.decode().strip().lower().split("\n")
-    print(output_lines)
+    return output
 
-get_coordinates()
+
+def _parse_coordinates(wherami_output: bytes) -> Coordinates:
+    try:
+        output = wherami_output.decode().strip().lower().split('\n')
+    except UnicodeDecodeError:
+        raise CantGetCoordinates
+    return Coordinates(latitude=_parse_coord(output, 'latitude'),
+                       longitude=_parse_coord(output, 'longitude'))
+
+
+def _parse_coord(output: list[str], coord_type: Literal['latitude'] | Literal['longitude']) -> float:
+    for line in output:
+        if line.startswith(f"{coord_type}:"):
+            return _parse_float_coordinate(line.split()[1])
+        else:
+            raise CantGetCoordinates
+
+
+def _parse_float_coordinate(value: str) -> float:
+    try:
+        return float(value)
+    except ValueError:
+        raise CantGetCoordinates
+
+
+def _round_coordiantes(coordinates: Coordinates) -> Coordinates:
+    if not config.USE_ROUNDED_COORDS:
+        return coordinates
+    return Coordinates(*map(lambda c: round(c, 1), [coordinates.latitude, coordinates.longitude]))
+
+if __name__ == "__main__":
+    print(get_gps_coordinates())
